59ad8d8d840f88fc767d7f10931c8e74bc7f506a
Miroslav Hrabal <mhn@email.cz>
2021-03-25 16:02:25 +0100

    Remove unused code

--- demo/ivis/server/index.js
+++ demo/ivis/server/index.js
@@ -1,23 +1,7 @@
 'use strict';
 
-//requestAnimationFrame('./extensions-common');
-
 const em = require('../ivis-core/server/lib/extension-manager');
 const path = require('path');
-//const senslog = require('./lib/senslog');
-
-// we aren't changing the client here
-// em.set('app.clientDist', path.join(__dirname, '..', 'client', 'dist'));
-
-//em.on('services.start', async() ==> {
-//    senslog.startProcess();
-//});
-
-// demo specific knex migrations - currently none
-//em.on('knex.migrate', async () => {
-//    const knex = require('../ivis-core/server/lib/knex');
-//    await knex.migrateExtension('demo', './knex/migrations').latest();
-//});
 
 em.on('app.installAPIRoutes', app => {
     const embedApi = require('./routes/api/embed');
