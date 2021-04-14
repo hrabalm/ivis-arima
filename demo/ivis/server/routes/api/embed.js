1e355aca16f8a0d468432a0d37139dd67d8fc02d
Miroslav Hrabal <mhn@email.cz>
2021-04-14 20:31:06 +0200

    Add API routes for adding signal sets and records

--- demo/ivis/server/routes/api/embed.js
+++ demo/ivis/server/routes/api/embed.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const passport = require('../../../ivis-core/server/lib/passport');
 const router = require('../../../ivis-core/server/lib/router-async').create();
+const signalSets = require('../../../ivis-core/server/models/signal-sets');
+const signals = require('../../../ivis-core/server/models/signals');
+const { castToInteger } = require('../../../ivis-core/server/lib/helpers');
+
+
+router.getAsync('/predictions/:modelId', passport.loggedIn, async (req, res) => {
+    return res.json();
+});
+
+router.postAsync('/signal-set/:signalSetCid', passport.loggedIn, async (req, res) => {
+    return res.json();
+});
+
+router.postAsync('/signal-sets', passport.loggedIn, async (req, res) => {
+    return res.json(await signalSets.create(req.context, req.body));
+});
+
+router.postAsync('/signals/:signalSetCid', passport.loggedIn, async (req, res) => {
+    const set = await signalSets.getByCid(req.context, req.params.signalSetCid);
+    console.log(await set.id);
+    return res.json(await signals.create(req.context, set.id, req.body));
+});
+
+router.postAsync('/signal-set-records/:signalSetId', passport.loggedIn, async (req, res) => {
+    const sigSetWithSigMap = await signalSets.getById(req.context, castToInteger(req.params.signalSetId), false, true);
+    await signalSets.insertRecords(req.context, sigSetWithSigMap, [req.body]);
+    return res.json();
+});
 
 module.exports = router;
