8fca86b2bcf9d0fd019955091bbed2e642f886ea
Miroslav Hrabal <mhn@email.cz>
2021-04-15 14:10:26 +0200

    WIP timestamp shifting

--- demo/data_uploader/uploader.py
+++ demo/data_uploader/uploader.py
@@ -77,27 +77,81 @@ def create_signal_set(cid, name, signals=[]):
 
 def upload_record(setId, record):
     resp = requests.post(API_BASE + '/signal-set-records/' + str(setId), headers=headers, json=record)
-    print(record)
+    print(resp.json())
 
-def process_csv_file(filename):
+def shift_csv_file(filename, output, shift='y', ts_field='ts'):
+    # firstly, find first and last timestamp
+    first_ts = None
+    last_ts = None
+    with open(filename, mode='r') as file:
+        reader = csv.DictReader(file, delimiter=',')
+        for row in reader:
+            if not first_ts:
+                first_ts = row[ts_field]
+            last_ts = row[ts_field]
+
+    first_ts = pendulum.parse(first_ts)
+    last_ts = pendulum.parse(last_ts)
+
+    # find proper shift
+    shifts = {
+        'd': pendulum.Duration(days=1),
+        'y': pendulum.duration(years=1)
+    }
+
+    mul = 0
+    delta = shifts[shift]
+    now = pendulum.now()
+
+    while first_ts + (mul * delta) < now and first_ts + ((mul + 1) * delta) < now:
+        mul += 1
+
+    # while (now - (first_ts+(mul+1)*delta))
+
+    delta *= mul
+
+    with open(filename, mode='r') as file, open(output, mode='w') as outfile:
+        reader = csv.DictReader(file, delimiter=',')
+        writer = csv.DictWriter(outfile, fieldnames=reader.fieldnames, dialect='excel')
+
+        writer.writeheader()
+        previous_ts = None
+        for row in reader:
+            ts = pendulum.parse(row[ts_field])+delta
+            row[ts_field] = ts.format(DATE_FORMAT)
+            if previous_ts != ts:
+                writer.writerow(row)
+            else:
+                pass  # throw away duplciatate timestamps (e.g. february 28 for years)
+            previous_ts = ts
+
+def wait_for_ts(ts):
+    import time
+    now = pendulum.now()
+    if ts > now:
+        print("Waiting", (ts-now).in_seconds(), "seconds.")
+        time.sleep((ts-now).in_seconds())
+
+def process_csv_file(set_cid, set_name, filename):
     ts_field = 'ts'
+
     with open(filename, mode='r') as file:
         reader = csv.DictReader(file, delimiter=',')
 
         for i, value in enumerate(reader):
             ts = value[ts_field]
             ts = pendulum.parse(ts)
-            print('ts:', ts)
+            #print('ts:', ts)
             signals = {k: v for k, v in value.items() if k != ts_field}
-            print('signals:', signals)
-            ts = ts.format(DATE_FORMAT)
+            #print('signals:', signals)
+            ts_str = ts.format(DATE_FORMAT)
 
             if i == 0:
-                name = filename.split('.')[0]
-                setId = create_signal_set(name, name, [k for k in signals])
-                upload_record(setId, {'id': ts, 'signals': {'ts': ts, **signals}})
+                setId = create_signal_set(set_cid, set_name, [k for k in signals])
+                upload_record(setId, {'id': ts_str, 'signals': {'ts': ts_str, **signals}})
             else:
-                upload_record(setId, {'id': ts, 'signals': {'ts': ts, **signals}})
+                wait_for_ts(ts)
+                upload_record(setId, {'id': ts_str, 'signals': {'ts': ts_str, **signals}})
 
 
 
@@ -123,4 +177,6 @@ if __name__ == '__main__':
                 key=API_KEY)
     main()
     #create_signal_set('testSignalSet', 'Test Signal Set', ['ahoj'])
-    process_csv_file('test.csv')
+    shift_csv_file('test.csv', 'shifted.csv')
+    #shift_csv_file('shifted.csv', 'shifted2.csv')
+    process_csv_file('co2-shifted-8', 'CO2 Shifted', 'shifted.csv')
