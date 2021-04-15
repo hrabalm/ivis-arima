91ba1cdf671b5340d9b1a3b617712cb5e4b01481
Miroslav Hrabal <mhn@email.cz>
2021-04-15 17:47:54 +0200

    Rename created signal sets

--- demo/data_uploader/uploader.py
+++ demo/data_uploader/uploader.py
@@ -6,18 +6,19 @@ import multiprocessing
 if __name__ == '__main__':
     hp.wait_for_ivis()
     hp.set_api_key(dbhost=hp.MYSQL_HOST,
-                dbport=hp.MYSQL_PORT,
-                dbuser=hp.MYSQL_USER,
-                dbpass=hp.MYSQL_PASS,
-                dbname=hp.MYSQL_DB,
-                key=hp.API_KEY)
+                   dbport=hp.MYSQL_PORT,
+                   dbuser=hp.MYSQL_USER,
+                   dbpass=hp.MYSQL_PASS,
+                   dbname=hp.MYSQL_DB,
+                   key=hp.API_KEY)
 
     def f():
-        hp.upload_csv_wait('wait5_'+str(uuid.uuid4()), 'With Wait', 'test.csv', 1000, wait_seconds=5)
+        hp.upload_csv_wait('co2w5_'+str(uuid.uuid4()),
+                           'CO2 Wait 5s', 'test.csv', 1000, wait_seconds=5)
 
     def g():
-        hp.upload_csv_wait('wait1_'+str(uuid.uuid4()), 'With Wait',
-                        'test.csv', 1000, wait_seconds=1)
+        hp.upload_csv_wait('co2w1_'+str(uuid.uuid4()), 'CO2 Wait 1s',
+                           'test.csv', 1000, wait_seconds=1)
 
     pm = hp.ProcessManager()
     pm.add_process(f)
