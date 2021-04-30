371c59a979fc3e85929566bdee25c74db4fa1d27
Miroslav Hrabal <mhn@email.cz>
2021-04-30 21:27:43 +0200

    Update docker files

--- demo/ivis/docker-entrypoint.sh
+++ demo/ivis/docker-entrypoint.sh
@@ -12,28 +12,5 @@ do
     sleep 1
 done
 
-# run knex seed not yet done
-(
-    if [ -e /seeded ]
-    then
-        echo "Skipping knex seeds and migrations as they have already been run."
-    else
-        cd knex
-        rm seeds/* # remove stock seeds
-        cp /seeds/* seeds/
-        ../node_modules/.bin/knex migrate:latest # we have to run migrations before seed
-        ../node_modules/.bin/knex seed:run
-        touch /seeded
-    fi
-)
-
-if [ $NODE_WATCH ]
-then
-    (
-        cd /opt/demo/ivis-core/client
-        npm run watch
-    ) &
-fi
-
 # start ivis server
-cd /opt/demo/server && sleep 1 && node index.js
+cd /opt/demo/server && node index.js
